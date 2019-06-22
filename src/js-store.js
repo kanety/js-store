dcbb2c7d2fc011ec08525343320acf55d4a6bc9d
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2019-06-22 19:30:07 +0900

    Remove global config

--- src/js-store.js
+++ src/js-store.js
@@ -1,11 +1,9 @@
-const DEFAULTS = {
-  type: 'session',
-  key: null,
-};
-
 export default class JsStore {
   constructor(opts = {}) {
-    this.opts = JsStore.assign({}, DEFAULTS, opts)
+    this.opts = {
+      type: opts.type || 'session',
+      key: opts.key
+    }
     this.inst = new WebStorage(this.opts);
   }
 
@@ -20,32 +18,14 @@ export default class JsStore {
   remove() {
     this.inst.remove(this.opts.key);
   }
-
-  static getDefaults() {
-    return DEFAULTS;
-  }
-
-  static setDefaults(opts) {
-    return JsStore.assign(DEFAULTS, opts);
-  }
-
-  static assign(...objs) {
-    return objs.reduce((ret, obj) => {
-      Object.keys(obj).forEach((key) => {
-        ret[key] = obj[key];
-      });
-      return ret;
-    });
-  }
 }
 
-class WebStorage {
+export class WebStorage {
   constructor(opts = {}) {
-    if (opts.type == 'local') {
-      this.storage = window.localStorage;
-    } else if (opts.type == 'session') {
-      this.storage = window.sessionStorage;
-    }
+    this.storage = {
+      local: window.localStorage,
+      session: window.sessionStorage
+    }[opts.type];
   }
 
   get(key) {
